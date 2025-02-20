112a9b684454accea8fdbb27ff784d300e4678d5
Rakshitha-JK <rakshithajk21@gmail.com>
2025-02-20 17:11:02 +0530

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,10 @@
+def question():
+
+    qb1 = input("Enter the first question: ")
+    qb2 = input("Enter the second question: ")
+
+  
+    print(f"How do {qb1} and {qb2} relate to each other? What are their roles in current technology.")
+
+
+question()
